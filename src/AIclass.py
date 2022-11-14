9026a229beb4d9fa7692ca4cd7ec3bf56a76c16a
Zacharie Lawson <zacharie2002@gmail.com>
2022-11-14 15:53:19 +0100

    feat (bases) : implementation of a basic ai that follow the protocol

--- src/AIclass.py
+++ src/AIclass.py
@@ -1,3 +1,5 @@
+import sys
+
 ALLY = 1
 ENEMY = -1
 EMPTY = 0
@@ -5,17 +7,18 @@ EMPTY = 0
 class ai:
     def __init__(self):
         self.board = []
-        self.turn = 0
+        self.current_turn = 0
         self.color = 0
         self.time = 0
         self.x = 0
         self.y = 0
         self.size = 0
+        self.game_info = {}
 
     ## protocol
     def start(self, size):
-        self.size = size
-        if (type(size) == int and size >= 0):
+        self.size = int(size)
+        if (type(self.size) == int and self.size >= 0):
             self.board = [[EMPTY for x in range(size)] for y in range(size)]
             print("OK", flush=True)
             return True
@@ -23,7 +26,7 @@ class ai:
             print("ERROR Invalid size (not positive number)", flush=True)
             return False
 
-    def turn(self, pos):
+    def turn(self, pos: str):
         try:
             x, y = map(int, pos.split(','))
             if (x < 0 or y < 0 or x >= self.size or y >= self.size):
@@ -33,17 +36,38 @@ class ai:
                 print("ERROR Invalid position (already taken)", flush=True)
                 return False
             else:
-                self.board[x][y] = self.color
-                self.turn += 1
-                print("OK", flush=True)
-                return True
+                self.board[x][y] = ENEMY
+                self.current_turn += 1
+                return self.do_action()
         except:
             print("ERROR Invalid position (not a number)", flush=True)
             return False
 
+    def begin(self):
+        self.do_action()
+    
+    def board(self):
+        while (input() != "DONE"):
+            pass
+        return self.do_action()
+
+    def info(self, info):
+        self.game_info[info[0]] = info[1]
+    
+    def end(self):
+        pass
+    
+    def about(self):
+        print("name=\"Gomoku\", version=\"1.0\", author=\"Gomoku\", country=\"France\"")
 
     ## Brain of the AI    
     def do_action(self):
-        while True:
-            print("im the AI, its my turn")
-            continue
+        for x in range(self.size):
+            for y in range(self.size):
+                if (self.board[x][y] == EMPTY):
+                    self.board[x][y] = ALLY
+                    self.current_turn += 1
+                    print(str(x) + "," + str(y), flush=True)
+                    return True
+        print("ERROR No more space", flush=True)
+        return False
--- src/gomoku.py
+++ src/gomoku.py
@@ -1,6 +1,20 @@
 from AIclass import ai
+import sys
 
 def gomoku():
     my_ai = ai()
     while True:
-        my_ai.do_action()
+        command = input().split()
+        if command[0] == "START":
+            my_ai.start(int(command[1]))
+        elif command[0] == "TURN":
+            my_ai.turn(str(command[1]))
+        elif command[0] == "BEGIN":
+            my_ai.begin()
+        elif command[0] == "BOARD":
+            my_ai.board()
+        elif command[0] == "INFO":
+            my_ai.info(command[1:])
+        elif command[0] == "END":
+            my_ai.end()
+            break
